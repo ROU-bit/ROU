f37e28d0f87a211b384c838a32bf2aefa092678d
86183 <csq@qq.com>
2025-03-01 12:29:26 +0800

    截止枚举

---
+++ Csharp基础教学/Homework1_枚举/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace Homework1_枚举
+{
+    #region 1 QQ状态
+    //定义QQ状态的枚举
+    enum E_qqState
+    {
+        Online,
+        Busy,
+        Quit,
+        Invisible,
+    }
+
+    #endregion
+    
+
+    #region 2 星巴克买咖啡
+
+    enum E_StarBacks
+    {
+        middleCup,
+        bigCup,
+        superBigCup,
+    }
+
+    #endregion
+
+
+    #region 3 RPG
+    enum E_HeroSex
+    {
+        Female,
+        Male,
+    }
+    enum E_HeroJob
+    {
+        Warrior,
+        Hunter,
+        Wizard,
+    }
+    #endregion
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 QQ状态
+
+            //提示用户选择一个在线状态，我们接受输入的数字，并将其转换成枚举类型
+            Console.WriteLine("请于在线状态‘0、1、2、3’中选择一个：");
+            int input = int.Parse(Console.ReadLine());
+            E_qqState state = (E_qqState)input;
+            Console.WriteLine(state);
+
+            #endregion
+
+
+            #region 2 星巴克买咖啡
+            /*
+              用户去星巴克买咖啡，分为中杯（35元），大杯（40元），超大杯（43元），
+              请用户选择要购买的类型，用户选择后，打印：您购买了
+              xxx咖啡，花费了xx元
+              例如：你购买了中杯咖啡，花费了35元
+            */
+
+            Console.WriteLine("请从'中杯0、大杯1、超大杯2'中选择您要购买的类型：");
+            char inputStarBacks = Console.ReadKey(true).KeyChar;
+            E_StarBacks starBacks = E_StarBacks.middleCup;
+            int starBackPrice = 0;
+            switch (inputStarBacks)
+            {
+                case '0':
+                    starBacks = E_StarBacks.middleCup;
+                    starBackPrice = 35;
+                    break;
+                case '1':
+                    starBacks = E_StarBacks.bigCup;
+                    starBackPrice = 40;
+                    break;
+                case '2':
+                    starBacks = E_StarBacks.superBigCup;
+                    starBackPrice = 43;
+                    break;
+            }
+            Console.WriteLine("您购买了{0}咖啡，花费了{1}元。", starBacks, starBackPrice);
+
+            #endregion
+
+
+            #region 3 RPG
+            Console.WriteLine("请选择英雄性别：男(0)、女(1)");
+            E_HeroSex heroSex = (E_HeroSex)int.Parse(Console.ReadKey(true).KeyChar.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("请选择英雄职业：战士(0)、猎人(1)、法师(2)");
+            E_HeroJob heroJob = (E_HeroJob)int.Parse(Console.ReadKey(true).KeyChar.ToString());
+            Console.WriteLine();
+
+            int atk = 0, def = 0;
+            string jobOutput = "";
+            string skill = "";
+
+            // 设置基础属性
+            switch (heroSex)
+            {
+                case E_HeroSex.Male:
+                    atk += 50;
+                    def += 100;
+                    break;
+                case E_HeroSex.Female:
+                    atk += 150;
+                    def += 20;
+                    break;
+            }
+
+            // 根据职业调整属性和技能
+            switch (heroJob)
+            {
+                case E_HeroJob.Warrior:
+                    atk += 20;
+                    def += 100;
+                    jobOutput = "战士";
+                    skill = "冲锋";
+                    break;
+                case E_HeroJob.Hunter:
+                    atk += 120;
+                    def += 30;
+                    jobOutput = "猎人";
+                    skill = "假死";
+                    break;
+                case E_HeroJob.Wizard:
+                    atk += 200;
+                    def += 10;
+                    jobOutput = "法师";
+                    skill = "奥术冲击";
+                    break;
+            }
+
+            Console.WriteLine("您选择了“{0}性{1}”，攻击力：{2}，防御力：{3}，职业技能：{4}",
+                heroSex == E_HeroSex.Male ? "男" : "女",
+                jobOutput,
+                atk,
+                def,
+                skill);
+
+            #endregion
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Csharp基础教学/Lesson1_枚举/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Lesson1_枚举
+{
+    #region 知识点一 基本概念
+
+    #region 1.枚举是什么
+    //枚举是一个比较特别的存在
+    //它是一个*被命名的整形常量的集合*
+    //一般用它来表示状态、类型等等
+    #endregion
+
+    #region 2.申明枚举和申明枚举变量的区别
+    //注意：申明枚举和申明枚举变量是两个概念
+    //申明枚举：相当于是创建一个自定义的枚举类型
+    //申明枚举变量：使用申明的自定义枚举类型来创建一个枚举变量
+    #endregion
+
+    #region 3.申明枚举语法
+    //枚举名以E或者E_开头 是命名规范
+    //根据公司要求来
+    /*
+    enum E_自定义枚举名
+    {
+      自定义枚举项名字0,      //枚举中包裹的整形常量 第一个默认值是0 下面的依次累加
+        自定义枚举项名字1,
+        自定义枚举项名字2,
+        自定义枚举项名字3,(最后一个逗号可省略)
+    }
+
+    enum E_自定义枚举名
+    {
+        自定义枚举项名字0 = 5,      //第一个枚举项的默认值为5，下面依次累加
+        自定义枚举项名字1,          //6
+        自定义枚举项名字2 = 100,    //100
+        自定义枚举项名字3,          //101       在上一个的基础上进行累加
+    }
+    */
+
+    #endregion
+
+    #endregion
+
+
+    #region 知识点二 在哪里申明枚举
+    //1.namespace语句块中 （常用）
+    //2.class语句块中
+    //3.struct语句块中
+    //注意：枚举不能在函数语句块中申明！！！
+
+    enum E_MonsterType
+    {
+        Normal,
+
+        Boss,
+    }
+
+    enum E_PlayerType
+    {
+        Main,
+
+        Other,
+    }
+
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("枚举");
+
+            #region 知识点三 枚举的使用
+            //申明枚举变量
+            //自定义的枚举类型 变量名 = 默认值;(自定义的枚举类型.枚举项)
+            E_PlayerType playerType = E_PlayerType.Main;
+
+            if(playerType == E_PlayerType.Main)
+            {
+                Console.WriteLine("主玩家逻辑");
+            }
+            else if(playerType == E_PlayerType.Other)
+            {
+                Console.WriteLine("其他玩家逻辑");
+            }
+
+            //枚举和switch是天生一对
+            E_MonsterType monsterType = E_MonsterType.Boss;
+            switch (monsterType)
+            {
+                case E_MonsterType.Normal:
+                    Console.WriteLine("普通怪物的逻辑");
+                    break;
+                case E_MonsterType.Boss:
+                    Console.WriteLine("boss怪的逻辑");
+                    break;
+                default:
+                    break;
+            }
+
+            #endregion
+
+
+            #region 知识点四 枚举的类型转换
+            //1.枚举和int互转
+            int i =(int)playerType;
+            Console.WriteLine(i);               //0
+            //int转枚举
+            playerType = 0;
+
+            //2.枚举和string相互转换
+            string str = playerType.ToString(); //打印枚举项的名字
+            Console.WriteLine(str);             //Main
+            //string转枚举
+            //Parse后第一个参数：你要转为哪个枚举类型
+                   //第二个参数：用于转换的对应枚举项的字符串
+            //转换完毕后是一个通用的类型 我们需要用括号强转成我们想要的目标枚举类型
+            playerType = (E_PlayerType)Enum.Parse(typeof(E_PlayerType),"Other");
+            Console.WriteLine(playerType);      //other
+
+            #endregion
+
+
+            #region 知识点五 枚举的作用
+            //在游戏开发中，对象很多时候会有许多状态
+            //比如玩家有一个动作状态 我们需要用一个变量或者标识来表示当前玩家处于的是哪种状态
+            //综合考虑 可能会使用int来表示他的状态
+            //1 行走 2 待机 3 跑步 4 跳跃 ......
+
+            //枚举可以帮助我们清晰的分清楚状态的含义
+
+            #endregion
+
+
+        }
+    }
+}
+
